860659acb60dc1493e71fe76dac85ecd145b073c
guilherme Douglas <gui08@gmail.com>
2025-02-14 15:16:44 -0300

    criando algumas coisas para renovar a parte do projeto com o main sendo excultado

--- src/App/App.java
+++ src/App/App.java
@@ -1,10 +1,24 @@
 package App;
 
+import Class.Assento;
+import Class.Pagamento;
+import Class.Passageiro;
+import Class.Reserva;
+import Class.Voo;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("ola mundo");
+        Passageiro passageiro = new Passageiro(null, 0, null, null, null, null);
 
+        Voo voo = new Voo(0, null, null, null, null, null, null, 0, null);
+
+        Reserva reserva = new Reserva(null, passageiro, voo, null, null, 0);
+
+        Assento assento = new Assento(0, null, null, null);
+
+        Pagamento pagamento = new Pagamento(null, null, null, null);
+
+        
     }
 }
